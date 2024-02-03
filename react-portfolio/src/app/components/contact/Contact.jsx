96d678323f9e32668d5a54f87675a0587b35a564
dgomez619 <50347479+dgomez619@users.noreply.github.com>
2024-02-02 22:17:22 -0400

    update2

--- react-portfolio/src/app/components/contact/Contact.jsx
+++ react-portfolio/src/app/components/contact/Contact.jsx
@@ -37,9 +37,9 @@ const Contact = () => {
                 <form ref={form} onSubmit={sendEmail} className='w-full p-8 my-4 md:px-12 lg:w-9/12 lg:pl-20 lg:pr-40 mr-auto rounded-2xl shadow-2xl'>
                     {submissionMessage && <div className="text-green-500 pb-5">{submissionMessage}</div>}
 
-                    <div className="">
+                    <div>
                         <div className="flex">
-                            <h1 className="font-bold uppercase text-5xl">Let's <br /> Connect</h1>
+                            <h1 className="font-bold uppercase text-5xl">Lets <br /> Connect</h1>
                         </div>
                         <div className="grid grid-cols-1 gap-5 md:grid-cols-2 mt-5">
 
